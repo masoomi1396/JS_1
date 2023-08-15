a6f251e768ffb1beed264afedfa369e0980fa501
Amirmohammad Masoumi <39368982+masoomi1396@users.noreply.github.com>
2023-08-15 11:08:41 +0330

    Add files via upload

--- master.js
+++ master.js
@@ -1,51 +1,36 @@
 let number1,number2;
 alert("1 == + , 2 == - , 3 == * , 4 == / , 5 == % , 6 == **");
 const whichAction = Number(prompt());
+alert("Enter two numbers:");
+number1 = Number(prompt());
+number2 = Number(prompt());
 if(whichAction == 1 )
 {
-    alert("Enter tow numbers:");
-    number1 = Number(prompt());
-    number2 = Number(prompt());
     const result = number1+number2;
     alert("Result == "+ result);
 }
 else if(whichAction == 2)
 {
-    alert("Enter tow numbers:");
-    number1 = Number(prompt());
-    number2 = Number(prompt());
     const result = number1-number2;
     alert("Result == "+ result);
 }
 else if(whichAction == 3)
 {
-    alert("Enter tow numbers:");
-    number1 = Number(prompt());
-    number2 = Number(prompt());
     const result = number1*number2;
     alert("Result == "+ result);
 }
 else if(whichAction == 4)
 {
-    alert("Enter tow numbers:");
-    number1 = Number(prompt());
-    number2 = Number(prompt());
     const result = number1/number2;
     alert("Result == "+ result);
 }
 else if(whichAction == 5)
 {
-    alert("Enter tow numbers:");
-    number1 = Number(prompt());
-    number2 = Number(prompt());
     const result = number1%number2;
     alert("Result == "+ result);
 }
 else if(whichAction == 6)
 {
-    alert("Enter tow numbers:");
-    number1 = Number(prompt());
-    number2 = Number(prompt());
     const result = number1**number2;
     alert("Result == "+ result);
 }
